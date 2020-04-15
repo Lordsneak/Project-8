cab16eddcbf7f7c72a714930f12c64582dc1607b
Hamza Ghounbaz <Hamza1ghounbaz@gmail.com>
2020-04-15 12:07:05 +0100

    Nodejs fix SQL

--- sqlfile/ismydata.sql
+++ sqlfile/ismydata.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 15, 2020 at 11:39 AM
+-- Generation Time: Apr 15, 2020 at 01:06 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.4
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `auteur` (
   `id` int(11) NOT NULL,
-  `nom` text NOT NULL
+  `nom` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -40,10 +40,26 @@ CREATE TABLE `auteur` (
 
 CREATE TABLE `citation` (
   `id` int(11) NOT NULL,
-  `text` text NOT NULL,
-  `source` text NOT NULL,
+  `text` varchar(255) NOT NULL,
+  `source` varchar(255) NOT NULL,
   `id_auteur` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `auteur`
+--
+ALTER TABLE `auteur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `citation`
+--
+ALTER TABLE `citation`
+  ADD PRIMARY KEY (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
